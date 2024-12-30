f071ed533efc23485b6a7c14efdeb280af46ac38
remizov.ms <remizov.ms@gdcloud.org>
2024-12-30 22:08:44 +0300

    fix code

--- src/components/VideoPlayerBase/VideoPalyerBase.jsx
+++ src/components/VideoPlayerBase/VideoPalyerBase.jsx
@@ -1,47 +1,46 @@
-import { useEffect, useRef } from 'react';
-import Script from 'next/script';
+import { useEffect, useRef } from "react";
+import Script from "next/script";
 
-export const VideoPalyerBase = ({videoUrl}) => {
-    const iframeRef = useRef(null);
-    const playerRef = useRef(null);
+export const VideoPalyerBase = ({ videoUrl }) => {
+  const iframeRef = useRef(null);
+  const playerRef = useRef(null);
 
-  
-    if (!videoUrl) return;
+  useEffect(() => {
+    // Проверяем, загружен ли скрипт
+    if (window.VK && iframeRef.current) {
+      console.log("###", iframeRef.current);
+      playerRef.current = window.VK.VideoPlayer(iframeRef.current);
 
-    useEffect(() => {
-        // Проверяем, загружен ли скрипт
-        if (window.VK && iframeRef.current) {
-            console.log("###", iframeRef.current)
-          playerRef.current = window.VK.VideoPlayer(iframeRef.current);
-          
-          // Замьютить видео
-          playerRef.current.mute();
-        }
-      }, []);
-  
-    return (
-      <>
-        <Script
-          src="https://vk.com/js/api/videoplayer.js"
-          strategy="afterInteractive"
-          onLoad={() => {
-            if (iframeRef.current) {
-              playerRef.current = window.VK.VideoPlayer(iframeRef.current);
-              playerRef.current.mute(); // Замьютить видео после загрузки
-            }
-          }}
-        />
-        <iframe 
-            ref={iframeRef}
-            style={{ height: "100vh", width: "100vw", border: 'none' }}
-            src={`${videoUrl}`}
-            frameborder="0" 
-            allow="autoplay; encrypted-media; fullscreen; clipboard-write; " 
-            webkitallowfullscreen 
-            mozallowfullscreen 
-            allowfullscreen
-            loading="lazy"
-        ></iframe>  
-      </>
-    );
-}
+      // Замьютить видео
+      playerRef.current.mute();
+    }
+  }, []);
+
+  if (!videoUrl) return;
+
+  return (
+    <>
+      <Script
+        src="https://vk.com/js/api/videoplayer.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          if (iframeRef.current) {
+            playerRef.current = window.VK.VideoPlayer(iframeRef.current);
+            playerRef.current.mute(); // Замьютить видео после загрузки
+          }
+        }}
+      />
+      <iframe
+        ref={iframeRef}
+        style={{ height: "100vh", width: "100vw", border: "none" }}
+        src={`${videoUrl}`}
+        frameborder="0"
+        allow="autoplay; encrypted-media; fullscreen; clipboard-write; "
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+        loading="lazy"
+      ></iframe>
+    </>
+  );
+};
